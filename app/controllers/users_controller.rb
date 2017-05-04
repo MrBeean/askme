ed557adf76183ca8a9b034fdc8e9d8a794c8ce3c
mrbeean_HP <MrSitiZ@yandex.ru>
2017-05-04 13:10:30 +0300

    add views and style for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,36 @@
-# Регистрация, авторизация
 class UsersController < ApplicationController
+  def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'https://cdn.meme.am/cache/instances/folder203/250x250/48506203/mr-bean-happy-wow.jpg'
+      ),
+      User.new(id: 2, name: 'Misha', username: 'aristofun')
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = "Привет из метода 'show'"
+    @user = User.new(
+      name: 'Андрей',
+      username: 'mrBeaN',
+      avatar_url: 'https://cdn.meme.am/cache/instances/folder203/250x250/48506203/mr-bean-happy-wow.jpg'
+    )
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('02.05.2017')),
+      Question.new(
+        text: 'В чем смысл жизни?', created_at: Date.parse('27.04.2017')
+      )
+    ]
+
+    @new_question = Question.new
   end
 end
