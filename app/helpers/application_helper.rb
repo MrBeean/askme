020ae43dd336d53e03792499eb38049cc99af75d
mrbeean_HP <MrSitiZ@yandex.ru>
2017-05-04 16:14:54 +0300

    add views and style for users questions count

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,32 @@ module ApplicationHelper
       asset_path 'avatar.png'
     end
   end
+
+  def sklonenie(number, krokodil, krokodila, krokodilov)
+    if number == nil || !number.is_a?(Numeric)
+      number = 0
+    end
+
+    ostatok = number % 10
+
+    if (number % 100) >= 11 && (number % 100) <= 14
+      return krokodilov
+    end
+
+    if number >= 11 && number <= 14
+      return krokodilov
+    end
+
+    if ostatok == 1
+      return krokodil
+    end
+
+    if ostatok >= 2 && ostatok <= 4
+      return krokodila
+    end
+
+    if ostatok > 4 || ostatok == 0
+      return krokodilov
+    end
+  end
 end
