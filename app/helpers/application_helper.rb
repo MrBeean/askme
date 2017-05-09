b26024bf1effc1d0c84b0c638f63b021b9545dfb
mrbeean_HP <MrSitiZ@yandex.ru>
2017-05-09 10:08:25 +0300

    add questions controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,11 @@ module ApplicationHelper
     end
   end
 
+  # Хелпер, рисующий span тэг с иконкой из font-awesome
+  def fa_icon(icon_class)
+    content_tag 'span', '', class: "fa fa-#{icon_class}"
+  end
+
   def sklonenie(number, krokodil, krokodila, krokodilov)
     if number == nil || !number.is_a?(Numeric)
       number = 0
