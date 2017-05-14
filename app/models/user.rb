530fa842bc3fe2c9ddef63ec5b36053eaa793933
mrbeean_HP <MrSitiZ@yandex.ru>
2017-05-14 23:20:59 +0300

    add questioner to question

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,6 @@ class User < ApplicationRecord
 
   # Связь
   has_many :questions, dependent: :destroy
-  has_many :questions, foreign_key: "author_id", dependent: :destroy
 
   # Проверки
   before_validation :username_downcase!
